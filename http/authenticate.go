dd877f6b601d35e5e5a879326d90d614809d1ee3
Ryan Loader <talk@ryanloader.me>
2018-06-23 18:13:11 +0100

    Parse form input

--- http/authenticate.go
+++ http/authenticate.go
@@ -1,14 +1,23 @@
 package http
 
 import (
+	"log"
 	"net/http"
 
 	sqrl "github.com/RaniSputnik/sqrl-go"
 )
 
+const xFormURLEncoded = "application/x-www-form-urlencoded"
+
 func Authenticate() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.UserAgent() != sqrl.V1 {
+		if r.UserAgent() != sqrl.V1 || r.Header.Get("Content-Type") != xFormURLEncoded {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if err := r.ParseForm(); err != nil {
+			log.Printf("Failed to parse form: %s", err)
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
--- http/authenticate_test.go
+++ http/authenticate_test.go
@@ -3,15 +3,18 @@ package http_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	sqlhttp "github.com/RaniSputnik/sqrl-go/http"
 )
 
 func TestAuthenticate(t *testing.T) {
+	const emptyBody = ""
+
 	t.Run("ReturnsBadRequestWhenUserAgentNotProvided", func(t *testing.T) {
 		h := sqlhttp.Authenticate()
-		w, r := setupAuthenticate()
+		w, r := setupAuthenticate(emptyBody)
 		r.Header.Del("User-Agent")
 
 		h.ServeHTTP(w, r)
@@ -23,8 +26,20 @@ func TestAuthenticate(t *testing.T) {
 
 	t.Run("ReturnsBadRequestWhenUserAgentIsNotSQRL1", func(t *testing.T) {
 		h := sqlhttp.Authenticate()
-		w, r := setupAuthenticate()
+		w, r := setupAuthenticate(emptyBody)
 		r.Header.Set("User-Agent", "SQRL/6")
+
+		h.ServeHTTP(w, r)
+
+		if expected := http.StatusBadRequest; w.Code != expected {
+			t.Errorf("Expected: %d, got: %d", expected, w.Code)
+		}
+	})
+
+	t.Run("ReturnsBadRequestWhenContentTypeIsNotFormEncoded", func(t *testing.T) {
+		h := sqlhttp.Authenticate()
+		w, r := setupAuthenticate(emptyBody)
+		r.Header.Set("Content-Type", "application/json")
 
 		h.ServeHTTP(w, r)
 
@@ -34,9 +49,10 @@ func TestAuthenticate(t *testing.T) {
 	})
 }
 
-func setupAuthenticate() (*httptest.ResponseRecorder, *http.Request) {
+func setupAuthenticate(body string) (*httptest.ResponseRecorder, *http.Request) {
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest(http.MethodPost, "/", nil)
+	r := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(body))
 	r.Header.Set("User-Agent", "SQRL/1")
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	return w, r
 }
