6d6d71c9f17cbf856da956093dc69bd7a911c98b
Ryan Loader <talk@ryanloader.me>
2018-07-29 15:45:27 +0100

    Basic server payload parsing

--- server.go
+++ server.go
@@ -1,7 +1,66 @@
 package sqrl
 
-// ServerMsg is either the base64 encoded sqrl:// URL
-// sent by the server to initiate the exchange OR
-// the exact value of the 'server' parameter from the
-// previous transaction.
-type ServerMsg string
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// ServerMsg is used to represent the values
+// sent from the server to the client.
+type ServerMsg struct {
+	Ver []string
+	Nut string // TODO: type Nut
+	Tif int
+	Qry string
+
+	// TODO: url - for any command other than query
+	// TODO: sin - Secret index
+	// TODO: suk - server unlock key
+	// TODO: ask - message text to display to user
+	// TODO: can - cancellation redirection URL
+
+	// TODO: additional parameters
+}
+
+// ParseServer decodes the base64 encoded server
+// parameter into the component parts.
+func ParseServer(raw string) (*ServerMsg, error) {
+	bytes, err := Base64.DecodeString(raw)
+	if err != nil {
+		return nil, err
+	}
+
+	form := strings.Split(string(bytes), "\r\n") // TODO: Move newline char to const
+	if len(form) < 4 {
+		return nil, errors.New("missing one or more required parameters (ver,nut,tif,qry)")
+	}
+
+	vals := map[string]string{}
+	for _, keyval := range form {
+		if keyval == "" {
+			continue
+		}
+
+		pair := strings.SplitN(keyval, "=", 2)
+		if len(pair) < 2 {
+			return nil, fmt.Errorf("invalid value '%s', should be in the form: key=value\\r\\n", keyval)
+		}
+		vals[pair[0]] = pair[1]
+	}
+
+	tifstr := vals["tif"]
+	tif, err := strconv.Atoi(tifstr)
+	if err != nil {
+		return nil, fmt.Errorf("required value 'tif' is invalid: '%s'", tifstr)
+	}
+
+	// TODO: Check supported version before parsing
+	return &ServerMsg{
+		Ver: strings.Split(vals["ver"], ","),
+		Nut: vals["nut"],
+		Tif: tif,
+		Qry: vals["qry"],
+	}, nil
+}
---
+++ server_test.go
@@ -0,0 +1,45 @@
+package sqrl_test
+
+import (
+	"testing"
+
+	"github.com/RaniSputnik/sqrl-go"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServerParse(t *testing.T) {
+	t.Run("ReturnsValidServerMsg", func(t *testing.T) {
+		testCases := []struct {
+			Input  string
+			Expect sqrl.ServerMsg
+		}{
+			{
+				Input: "dmVyPTENCm51dD1iUlctSWVnQ1VoR21jejl5dlR0REtBDQp0aWY9NQ0KcXJ5PS9zcXJsP251dD1iUlctSWVnQ1VoR21jejl5dlR0REtBDQo",
+				Expect: sqrl.ServerMsg{
+					Ver: []string{sqrl.V1},
+					Nut: "bRW-IegCUhGmcz9yvTtDKA",
+					Tif: 5,
+					Qry: "/sqrl?nut=bRW-IegCUhGmcz9yvTtDKA",
+				},
+			},
+			{
+				Input: "dmVyPTENCm51dD1RTFlOd1N2TEZMZWd3RTlVMUZySG5BDQp0aWY9NA0KcXJ5PS9zcXJsP251dD1RTFlOd1N2TEZMZWd3RTlVMUZySG5BDQpzaW49MA0K",
+				Expect: sqrl.ServerMsg{
+					Ver: []string{sqrl.V1},
+					Nut: "QLYNwSvLFLegwE9U1FrHnA",
+					Tif: 4,
+					Qry: "/sqrl?nut=QLYNwSvLFLegwE9U1FrHnA",
+					// TODO: Sin: 0,
+				},
+			},
+		}
+
+		for _, test := range testCases {
+			got, err := sqrl.ParseServer(test.Input)
+			assert.NoError(t, err)
+			if assert.NotNil(t, got) {
+				assert.Equal(t, test.Expect, *got)
+			}
+		}
+	})
+}
