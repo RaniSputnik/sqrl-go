6042ea0dcda3608f2e525122e5123af9db1fb1c3
Ryan Loader <talk@ryanloader.me>
2019-01-13 18:28:13 +0000

    Add test for duplicate nuts

--- ssp/handler_test.go
+++ ssp/handler_test.go
@@ -19,12 +19,15 @@ func TestHandlerNutIsReturned(t *testing.T) {
 	res, err := http.Get(s.URL + "/nut.json")
 
 	// Assert no errors
-	fatal(t, assert.NoError(t, err, "Expected no HTTP/connection error"))
+	fatal(t, assert.NoError(t, err,
+		"Expected no HTTP/connection error"))
 	defer res.Body.Close()
 
 	// Assert headers
-	assert.Equal(t, http.StatusOK, res.StatusCode, "Expected successful status code")
-	assert.True(t, strings.HasPrefix("application/json", res.Header.Get("Content-Type")), "Expected response to have Content-Type 'application/json'")
+	assert.Equal(t, http.StatusOK, res.StatusCode,
+		"Expected successful status code")
+	assert.True(t, strings.HasPrefix("application/json", res.Header.Get("Content-Type")),
+		"Expected response to have Content-Type 'application/json'")
 
 	// Assert response body
 	type nutRes struct {
@@ -32,11 +35,37 @@ func TestHandlerNutIsReturned(t *testing.T) {
 	}
 	var got nutRes
 	err = json.NewDecoder(res.Body).Decode(&got)
-	fatal(t, assert.NoError(t, err, "Expected body to be decoded as JSON successfully"))
-	assert.NotEmpty(t, got.Nut, "Expected nut to be returned")
+	fatal(t, assert.NoError(t, err,
+		"Expected body to be decoded as JSON successfully"))
+	assert.NotEmpty(t, got.Nut,
+		"Expected nut to be returned")
 }
 
-// TODO: Test the same nut is not returned twice
+func TestHandlerNutIsUnique(t *testing.T) {
+	s := httptest.NewServer(ssp.Handler(anyKey))
+	endpoint := s.URL + "/nut.json"
+
+	type nutRes struct {
+		Nut string `json:"nut"`
+	}
+
+	results := make(map[string]bool)
+
+	for i := 0; i < 10; i++ {
+		res, err := http.Get(endpoint)
+		fatal(t, assert.NoError(t, err,
+			"Expected no HTTP/connection error"))
+		defer res.Body.Close()
+
+		var got nutRes
+		fatal(t, assert.NoError(t, json.NewDecoder(res.Body).Decode(&got),
+			"Expected body to be decoded as JSON successfully"))
+
+		seenBefore := results[got.Nut]
+		fatal(t, assert.False(t, seenBefore, "Duplicate nut returned: '%s'", got.Nut))
+		results[got.Nut] = true
+	}
+}
 
 func fatal(t *testing.T, ok bool) {
 	if !ok {
