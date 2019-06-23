36a71697b5bccff476a2d0df11400fda6287362e
Ryan Loader <talk@ryanloader.me>
2019-06-23 19:19:29 +0100

    Add token endpoint tests

--- ssp/handler_token_test.go
+++ ssp/handler_token_test.go
@@ -1,6 +1,7 @@
 package ssp_test
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -12,22 +13,92 @@ import (
 )
 
 func TestTokenHandler(t *testing.T) {
+	runHandler := func(h http.Handler) *httptest.ResponseRecorder {
+		r := httptest.NewRequest(http.MethodGet, "/token", nil)
+		w := httptest.NewRecorder()
+		h.ServeHTTP(w, r)
+		return w
+	}
+
 	t.Run("FailsWith401IfAuthFunctionRejects", func(t *testing.T) {
 		rejectAll := func(_ *http.Request) error { return errors.New("reject everyone") }
 		tokens := ssp.DefaultExchange(make([]byte, 16), time.Minute)
 
 		h := anyServer().WithAuthentication(rejectAll).TokenHandler(tokens)
-		r := httptest.NewRequest(http.MethodGet, "/token", nil)
-		w := httptest.NewRecorder()
+		result := runHandler(h)
 
-		h.ServeHTTP(w, r)
-
-		assert.Equal(t, http.StatusUnauthorized, w.Code)
+		assert.Equal(t, http.StatusUnauthorized, result.Code)
 	})
 
-	// TODO: FailsWith404IfTokenInvalid
+	t.Run("FailsWith404IfTokenInvalid", func(t *testing.T) {
+		validator := NewTokenValidator().ReturnsValidationError(ssp.ErrTokenFormatInvalid)
 
-	// TODO: Returns200IfTokenValid
+		h := anyServer().TokenHandler(validator)
+		result := runHandler(h)
 
-	// TODO: ReturnsUserFromTokenIfTokenValid
+		assert.Equal(t, http.StatusNotFound, result.Code)
+	})
+
+	t.Run("Returns200IfTokenValid", func(t *testing.T) {
+		validator := NewTokenValidator().ReturnsUser("someUser")
+
+		h := anyServer().TokenHandler(validator)
+		result := runHandler(h)
+
+		assert.Equal(t, http.StatusOK, result.Code)
+	})
+
+	t.Run("ReturnsUserFromTokenIfTokenValid", func(t *testing.T) {
+		type tokenResponse struct {
+			User string `json:"user"`
+		}
+
+		expectedUser := "someUser"
+		validator := NewTokenValidator().ReturnsUser(expectedUser)
+
+		h := anyServer().TokenHandler(validator)
+		result := runHandler(h)
+
+		var response tokenResponse
+		err := json.NewDecoder(result.Body).Decode(&response)
+
+		if assert.Nil(t, err) {
+			assert.Equal(t, expectedUser, response.User)
+		}
+	})
+}
+
+type mockTokenValidator struct {
+	Func struct {
+		Validate struct {
+			CalledWith struct {
+				Token ssp.Token
+			}
+			Returns struct {
+				UserID string
+				Err    error
+			}
+		}
+	}
+}
+
+func (m *mockTokenValidator) Validate(token ssp.Token) (string, error) {
+	m.Func.Validate.CalledWith.Token = token
+	return m.Func.Validate.Returns.UserID, m.Func.Validate.Returns.Err
+}
+
+func (m *mockTokenValidator) ReturnsValidationError(err error) *mockTokenValidator {
+	m.Func.Validate.Returns.UserID = ""
+	m.Func.Validate.Returns.Err = err
+	return m
+}
+
+func (m *mockTokenValidator) ReturnsUser(userID string) *mockTokenValidator {
+	m.Func.Validate.Returns.UserID = userID
+	m.Func.Validate.Returns.Err = nil
+	return m
+}
+
+func NewTokenValidator() *mockTokenValidator {
+	return &mockTokenValidator{}
 }
