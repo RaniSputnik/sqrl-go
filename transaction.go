fca1d49279be40f62e720da46e94e746ef8671a8
Ryan Loader <talk@ryanloader.me>
2019-08-17 12:52:30 +0100

    Add verify method to sqrl package

--- transaction.go
+++ transaction.go
@@ -1,7 +1,88 @@
 package sqrl
 
+import (
+	"errors"
+	"net/url"
+	"strings"
+)
+
+var (
+	ErrInvalidIDSig = errors.New("invalid identity signature")
+)
+
 type Transaction struct {
 	Client string
 	Server string
-	Ids    string
+	Ids    Signature
+	// TODO: Pids
+	// TODO: Suk
+
+	ClientIP string
+}
+
+// Verify checks that a transaction (request from a SQRL client) is valid
+//
+// A previous transaction should be provided if one exists. If no previous
+// transaction is provided, the request is presumed to be the first transaction.
+//
+// If an error is encoutered, the precise error will be returned and the
+// correct transaction information flags will be set on the response.
+func Verify(t *Transaction, prev *Transaction, response *ServerMsg) error {
+	client, errc := ParseClient(t.Client)
+	if errc != nil {
+		response.Tif = response.Tif | TIFCommandFailed | TIFClientFailure
+		return errors.New("invalid client param")
+	}
+	serverOK := verifyServer(t.Server, prev)
+	if !serverOK {
+		response.Tif = response.Tif | TIFCommandFailed | TIFClientFailure
+		return errors.New("invalid server param")
+	}
+	signedPayload := t.Client + t.Server
+	if !t.Ids.Verify(client.Idk, signedPayload) {
+		response.Tif = response.Tif | TIFCommandFailed | TIFClientFailure
+		return ErrInvalidIDSig
+	}
+
+	// TODO: Verify IP Match
+
+	// TODO: Verify IDK Match
+
+	return nil
+}
+
+func verifyServer(serverRaw string, prev *Transaction) bool {
+	// TODO: Here we accept EITHER a URL or ServerMsg
+	// However we know that ONLY the first request
+	// from the client should be a URL.
+	// Is there a way for us to ensure that here?
+
+	bytes, err := Base64.DecodeString(serverRaw)
+	if err != nil {
+		return false
+	}
+
+	server := string(bytes)
+	if strings.HasPrefix(server, "sqrl") {
+		serverURL, err := url.Parse(server)
+		if err != nil {
+			return false
+		}
+
+		// TODO: Assert URL matches server configuration
+		// eg. domain, "server friendly name", etc.
+
+		nut := serverURL.Query().Get("nut")
+		if nut == "" {
+			return false
+		}
+		return true
+
+	} else {
+		msg, err := ParseServer(serverRaw)
+		if err != nil || msg == nil || msg.Nut == "" {
+			return false
+		}
+		return true
+	}
 }
---
+++ transaction_test.go
@@ -0,0 +1,78 @@
+package sqrl_test
+
+import (
+	"testing"
+
+	"github.com/RaniSputnik/sqrl-go"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/ed25519"
+)
+
+func TestVerifyFirstTransaction(t *testing.T) {
+	alice, aliceSig := newIDKey()
+
+	c := &sqrl.ClientMsg{
+		Ver: []string{sqrl.V1},
+		Cmd: sqrl.CmdIdent,
+		Idk: alice,
+	}
+	validClient, _ := c.Encode()
+	validServer := sqrl.Base64.EncodeToString([]byte("sqrl://example.com/sqrl?nut=123456789"))
+	validIds := signature(aliceSig, validClient+validServer)
+
+	newResponse := func() *sqrl.ServerMsg {
+		return &sqrl.ServerMsg{ /* TODO */ }
+	}
+
+	t.Run("FailsWhenIDSInvalid", func(t *testing.T) {
+		transaction := &sqrl.Transaction{
+			Client:   validClient,
+			Server:   validServer,
+			Ids:      "invalid-sig",
+			ClientIP: "10.0.0.1",
+		}
+
+		err := sqrl.Verify(transaction, nil, newResponse())
+		assert.Equal(t, sqrl.ErrInvalidIDSig, err)
+	})
+
+	t.Run("FailsWhenSignedWrongPayload", func(t *testing.T) {
+		wrongPayload := validClient + "-" + validServer
+		invalidIds := signature(aliceSig, wrongPayload)
+
+		transaction := &sqrl.Transaction{
+			Client:   validClient,
+			Server:   validServer,
+			Ids:      invalidIds,
+			ClientIP: "10.0.0.1",
+		}
+
+		err := sqrl.Verify(transaction, nil, newResponse())
+		assert.Equal(t, sqrl.ErrInvalidIDSig, err)
+	})
+
+	t.Run("ReturnsNoErrorForAValidRequest", func(t *testing.T) {
+		transaction := &sqrl.Transaction{
+			Client:   validClient,
+			Server:   validServer,
+			Ids:      validIds,
+			ClientIP: "10.0.0.1",
+		}
+
+		err := sqrl.Verify(transaction, nil, newResponse())
+		assert.NoError(t, err)
+	})
+}
+
+func newIDKey() (sqrl.Identity, []byte) {
+	pub, priv, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		panic(err)
+	}
+	return sqrl.Identity(sqrl.Base64.EncodeToString(pub)), priv
+}
+
+func signature(privateKey ed25519.PrivateKey, payload string) sqrl.Signature {
+	sig := sqrl.Base64.EncodeToString(ed25519.Sign(privateKey, []byte(payload)))
+	return sqrl.Signature(sig)
+}
